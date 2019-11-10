e1de6ec0a58d39abe08ff78dee150bf61732aa1e
Kurtis Hill <kurtis.hill212@gmail.com>
2019-11-09 20:22:28 -0700

    Made styling changes to event dialog

--- lib/widgets/event_dialog.dart
+++ lib/widgets/event_dialog.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:time_budget/strings.dart';
-import 'package:intl/intl.dart';
+import 'package:time_budget/utils/date.dart';
 
 class EventDialog extends StatefulWidget {
   @override
@@ -18,6 +18,164 @@ class _EventDialogState extends State<EventDialog> {
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  Widget _buildTimeRange() {
+    return Container(
+      padding: const EdgeInsets.all(16.0),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Theme.of(context).primaryColor,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(10.0),
+          topRight: Radius.circular(10.0),
+        ),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              _selectDate(
+                  initialDate: _data['startTime'],
+                  onDateTimeChanged: (DateTime newDate) {
+                    _data['startTime'] = newDate;
+                  });
+            },
+            child: Column(
+              children: <Widget>[
+                Text(
+                  DateUtils.toyMMMdString(
+                    _data['startTime'],
+                  ),
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  DateUtils.toClockTime(
+                    _data['startTime'],
+                  ),
+                  style: TextStyle(
+                    color: Colors.grey[200],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Text(
+            '-',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 25,
+            ),
+          ),
+          GestureDetector(
+            onTap: () {
+              _selectDate(
+                  initialDate: _data['endTime'],
+                  onDateTimeChanged: (DateTime newDate) {
+                    _data['endTime'] = newDate;
+                  });
+            },
+            child: Column(
+              children: <Widget>[
+                Text(
+                  DateUtils.toyMMMdString(
+                    _data['endTime'],
+                  ),
+                  style: TextStyle(
+                    fontSize: 17,
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  DateUtils.toClockTime(
+                    _data['endTime'],
+                  ),
+                  style: TextStyle(
+                    color: Colors.grey[200],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNameTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: Strings.name,
+      ),
+      textInputAction: TextInputAction.next,
+      validator: (value) {
+        return value.isEmpty ? 'Event Name must not be empty' : null;
+      },
+      onSaved: (value) {
+        _data['eventName'] = value;
+      },
+    );
+  }
+
+  Widget _buildDescriptionTextField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: Strings.description,
+      ),
+      textInputAction: TextInputAction.next,
+      keyboardType: TextInputType.multiline,
+      maxLines: null,
+      onSaved: (value) {
+        _data['eventDescription'] = value;
+      },
+    );
+  }
+
+  Widget _buildCancelButton() {
+    return FlatButton(
+      child: Text(
+        'Cancel',
+        style: TextStyle(
+          color: Theme.of(context).primaryColorLight,
+        ),
+      ),
+      onPressed: () => Navigator.of(context).pop(null),
+    );
+  }
+
+  Widget _buildCreateButton() {
+    return RaisedButton(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(30),
+        side: BorderSide(
+          color: Theme.of(context).accentColor,
+        ),
+      ),
+      color: Colors.white,
+      child: Text(
+        'Create',
+        style: TextStyle(
+          color: Theme.of(context).accentColor,
+        ),
+      ),
+      onPressed: () {
+        if (!_formKey.currentState.validate()) {
+          return null;
+        }
+        _formKey.currentState.save();
+        return Navigator.of(context).pop(_data);
+      },
+    );
+  }
+
   Widget _buildEventDialog(BuildContext context) {
     return Card(
       elevation: 5,
@@ -25,95 +183,40 @@ class _EventDialogState extends State<EventDialog> {
         borderRadius: BorderRadius.circular(10.0),
       ),
       child: Padding(
-        padding: const EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 10.0),
+        padding: const EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 15.0),
         child: Form(
           key: _formKey,
           child: SingleChildScrollView(
             physics: NeverScrollableScrollPhysics(),
             child: Column(
               children: <Widget>[
-                TextFormField(
-                    decoration: InputDecoration(labelText: Strings.name),
-                    textInputAction: TextInputAction.next,
-                    onFieldSubmitted: (_) => {},
-                    validator: (value) {
-                      return value.isEmpty
-                          ? 'Event Name must not be empty'
-                          : null;
-                    },
-                    onSaved: (value) {
-                      _data['eventName'] = value;
-                    }),
-                TextFormField(
-                    decoration: InputDecoration(
-                      labelText: Strings.description,
-                    ),
-                    textInputAction: TextInputAction.next,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: 4,
-                    onFieldSubmitted: (_) => {},
-                    validator: (value) => null,
-                    onSaved: (value) {
-                      _data['eventDescription'] = value;
-                    }),
-                GestureDetector(
-                  onTap: () {
-                    _selectDate(
-                        initialDate: _data['startTime'],
-                        onDateTimeChanged: (DateTime newDate) {
-                          _data['startTime'] = newDate;
-                        });
-                  },
-                  child: Row(
-                    children: <Widget>[
-                      Text('Start Time'),
-                      Spacer(),
-                      Text(DateFormat('EEE, MMM d, yyyy - hh:mm aaa')
-                          .format(_data['startTime'])),
-                    ],
-                  ),
+                _buildTimeRange(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: _buildNameTextField(),
                 ),
-                GestureDetector(
-                  onTap: () {
-                    _selectDate(
-                        initialDate: _data['endTime'],
-                        onDateTimeChanged: (DateTime newDate) {
-                          _data['endTime'] = newDate;
-                        });
-                  },
-                  child: Row(
-                    children: <Widget>[
-                      Text('End Time'),
-                      Spacer(),
-                      Text(DateFormat('EEE, MMM d, yyyy - hh:mm aaa')
-                          .format(_data['endTime'])),
-                    ],
-                  ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                  child: _buildDescriptionTextField(),
                 ),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: <Widget>[
-                    FlatButton(
-                      child: Text('Cancel'),
-                      onPressed: () => Navigator.of(context).pop(null),
-                    ),
-                    FlatButton(
-                      child: Text(
-                        'Create',
-                        style: TextStyle(
-                          color: Theme.of(context).primaryColor,
+                SizedBox(
+                  height: 20,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: <Widget>[
+                      _buildCancelButton(),
+                      Expanded(
+                        child: ButtonTheme(
+                          minWidth: MediaQuery.of(context).size.width / 2,
+                          height: 45,
+                          child: _buildCreateButton(),
                         ),
                       ),
-                      onPressed: () {
-                        if (!_formKey.currentState.validate()) {
-                          return null;
-                        }
-                        _formKey.currentState.save();
-                        return Navigator.of(context).pop(_data);
-                      },
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ],
             ),
@@ -131,7 +234,10 @@ class _EventDialogState extends State<EventDialog> {
       ),
       elevation: 0.0,
       backgroundColor: Colors.transparent,
-      child: _buildEventDialog(context),
+      child: GestureDetector(
+        onTap: () => FocusScope.of(context).requestFocus(FocusNode()),
+        child: _buildEventDialog(context),
+      ),
     );
   }
 
@@ -143,7 +249,7 @@ class _EventDialogState extends State<EventDialog> {
       context: context,
       builder: (BuildContext context) {
         return Container(
-          height: MediaQuery.of(context).copyWith().size.height / 3,
+          height: MediaQuery.of(context).size.height / 3,
           child: CupertinoDatePicker(
             initialDateTime: initialDate,
             onDateTimeChanged: (DateTime newEndTime) {
